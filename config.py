38cef71f76b99364c04ec5bf2e32f2b254a65ca3
adriangohjw <adriangohjw@gmail.com>
2020-04-04 21:02:01 +0800

    update .gitignore and remove config file

--- config.py
+++
@@ -1,7 +0,0 @@
-POSTGRES_USER = 'postgres'
-POSTGRES_PW = 'localhostdbpassword'
-POSTGRES_URL = '127.0.0.1:5432'
-POSTGRES_DB = 'cz2006'
-
-class Config:
-    URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
