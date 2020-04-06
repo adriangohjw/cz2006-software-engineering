43cd8e2fda902741282c8126df5d192efef740d8
adriangohjw <adriangohjw@gmail.com>
2020-04-07 04:21:14 +0800

    add userAPI_byID to user_controllers

--- controllers/user_controllers.py
+++ controllers/user_controllers.py
@@ -5,7 +5,8 @@ from flask.helpers import make_response
 from exceptions import ErrorWithCode
 
 from contracts.user_contracts import \
-    user_create_contract, user_read_contract, user_update_contract, user_update_password_contract
+    user_create_contract, user_read_contract, user_update_contract, user_update_password_contract, \
+    user_read_contract_byID
     
 from operations.user_operations import \
     user_create_operation, user_read_operation, user_update_operation, user_update_operation_operation
@@ -28,7 +29,7 @@ class userAPI(Resource):
         # operations
         try:
             user = user_read_operation(
-                col='id', value=u['id']
+                col='username', value=u['username']
             )
         except ErrorWithCode as e:
             return make_response(
@@ -103,33 +104,65 @@ class userAPI(Resource):
         )
 
 
+class userAPI_byID(Resource):
+
+    def get(self):
+        
+        # contracts
+        try:
+            u = user_read_contract_byID(request)
+        except Exception as e:
+            return make_response(
+                jsonify (
+                    error = str(e),
+                ), 400
+            )
+
+        # operations
+        try:
+            user = user_read_operation(
+                col='id', value=u['id']
+            )
+        except ErrorWithCode as e:
+            return make_response(
+                jsonify (
+                    error = e.message
+                ), e.status_code
+            )
+        
+        # success case
+        return make_response(
+            jsonify (user.asdict()), 200
+        )
+
+
 class userPasswordAPI(Resource):
 
     def put(self):
 
-            # contracts
-            try:
-                u = user_update_password_contract(request)
-            except Exception as e:
-                return make_response(
-                    jsonify (
-                        error = str(e),
-                    ), 400
-                )    
-            
-            # operations
-            try:
-                user = user_update_operation_operation(
-                    u['id'], u['current_password'], u['new_password']
-                )
-            except ErrorWithCode as e:
-                return make_response(
-                    jsonify (
-                        error = e.message
-                    ), e.status_code
-                )
-            
-            # success case
+        # contracts
+        try:
+            u = user_update_password_contract(request)
+        except Exception as e:
             return make_response(
-                jsonify(user.asdict()), 200
+                jsonify (
+                    error = str(e),
+                ), 400
+            )    
+        
+        # operations
+        try:
+            user = user_update_operation_operation(
+                u['id'], u['current_password'], u['new_password']
             )
+        except ErrorWithCode as e:
+            return make_response(
+                jsonify (
+                    error = e.message
+                ), e.status_code
+            )
+        
+        # success case
+        return make_response(
+            jsonify(user.asdict()), 200
+        )
