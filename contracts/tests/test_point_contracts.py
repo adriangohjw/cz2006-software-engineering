3d1bc551862b2a6df04f222cd9a3d66f87233f25
adriangohjw <adriangohjw@gmail.com>
2020-04-07 20:22:58 +0800

    add unittest for user_contracts

---
+++ contracts/tests/test_point_contracts.py
@@ -0,0 +1,93 @@
+import sys
+from os import path, getcwd
+from flask import Flask
+from flask import request
+sys.path.append(getcwd())
+
+import unittest
+from models import db
+
+from run_test import create_app
+app = create_app()
+app.app_context().push()
+db.init_app(app)
+
+from contracts.point_contracts import \
+    validate_id, validate_latitude, validate_longtitude, \
+    point_read_contract, point_create_contract
+
+
+class Test_point_contracts(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("\n\n{}: starting test...".format(path.basename(__file__)))
+
+
+    def test_validate_id(self):
+        
+        with self.assertRaises(TypeError):
+            validate_id(None)
+            validate_id(1)
+
+        with self.assertRaises(ValueError):
+            validate_id('')
+
+
+    def test_validate_latitude(self):
+        
+        with self.assertRaises(TypeError):
+            validate_latitude(None)
+            validate_latitude(1.0)
+
+        with self.assertRaises(ValueError):
+            validate_latitude('')
+
+
+    def test_validate_longtitude(self):
+        
+        with self.assertRaises(TypeError):
+            validate_longtitude(None)
+            validate_longtitude(1.0)
+
+        with self.assertRaises(ValueError):
+            validate_longtitude('')
+
+
+    def test_point_read_contract(self):
+
+        with app.test_request_context('/?id=1', method='GET'):
+            self.assertEqual(
+                point_read_contract(request), 
+                {
+                    'id': 1
+                }
+            )
+
+        with app.test_request_context('/?id=', method='GET'):
+            self.assertRaises(TypeError, point_read_contract, request)
+
+        with app.test_request_context('/?id=hello', method='GET'):
+            self.assertRaises(TypeError, point_read_contract, request)
+
+
+    def test_point_create_contract(self):
+
+        with app.test_request_context('/?latitude=10.0&longtitude=20.0', method='POST'):
+            self.assertEqual(
+                point_create_contract(request), 
+                {
+                    'latitude': 10.0,
+                    'longtitude': 20.0
+                }
+            )
+
+        with app.test_request_context('/?latitude=&longtitude=', method='POST'):
+            self.assertRaises(TypeError, point_read_contract, request)
+
+        with app.test_request_context('/?latitude=hello&longtitude=20.0', method='POST'):
+            self.assertRaises(TypeError, point_read_contract, request)
+
+
+if __name__ == '__main__':
+    unittest.main()
