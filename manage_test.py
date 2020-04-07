5786a13ba226bdf4fbbd6d1a4055fb60e6135862
adriangohjw <adriangohjw@gmail.com>
2020-04-07 20:23:29 +0800

    initialize unittest

---
+++ manage_test.py
@@ -0,0 +1,16 @@
+from models import db
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+from run_test import create_app
+
+app = create_app()
+db.init_app(app)
+migrate = Migrate(app, db, compare_type=True)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == "__main__":
+    manager.run()
+    
