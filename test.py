24707c9c98b36e2a32f6f9443c7dad8bf1416faf
adriangohjw <adriangohjw@gmail.com>
2020-04-08 03:54:15 +0800

    add unittest for operations to main test.py

--- test.py
+++ test.py
@@ -11,5 +11,9 @@ from contracts.tests.test_point_contracts import Test_point_contracts
 from contracts.tests.test_route_contracts import Test_route_contracts
 from contracts.tests.test_algo_contracts import Test_algo_contracts
 
+from operations.tests.test_user_operations import Test_users_operations
+from operations.tests.test_route_operations import Test_route_operations
+from operations.tests.test_point_operations import Test_point_operations
+
 if __name__ == '__main__':
     unittest.main()
