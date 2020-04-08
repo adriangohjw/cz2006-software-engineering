b1fe2fa99e498bed7920a7af31edbf654158c12c
hitesh <hitesh001@e.ntu.edu.sg>
2020-04-08 19:01:51 +0800

    fixed bugs in frontend

--- Frontend/lib/models/user.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Model {
-  String firstName;
-  String lastName;
-  String email;
-  String password;
-Model({this.firstName, this.lastName, this.email, this.password});
-}
