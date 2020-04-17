614dbe1655261f832a2577f28049321f8ba84953
adriangohjw <adriangohjw@gmail.com>
2020-04-17 14:24:00 +0800

    add controllers (and IT) for stat

---
+++ controllers/tests/test_stat_controllers.py
@@ -0,0 +1,47 @@
+import sys
+from os import path, getcwd
+sys.path.append(getcwd())
+
+from flask import request
+import unittest
+
+from models import db, User, Route, Point
+from run_test import create_app
+
+from test_BaseCase import Test_BaseCase, res_to_dict
+
+from operations.user_operations import encrypt
+
+
+class Test_stat_controllers(Test_BaseCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("\n\n{}: starting test...".format(path.basename(__file__)))
+
+    
+    def test_dailyCaloriesAPI_GET(self):
+
+        import datetime
+        date_today = datetime.date.today()
+        date_today_str = date_today.strftime('%Y-%m-%d')
+
+        # invalid params input
+        response = self.app.get('/stats/daily_calories?user_id=')
+        self.assertEqual(response.status_code, 400)
+
+        # user not found
+        response = self.app.get('/stats/daily_calories?user_id=2')
+        self.assertEqual(response.status_code, 409)
+        
+        # success case
+        response = self.app.get('/stats/daily_calories?user_id=1')
+        res = res_to_dict(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(res['activities'][0]['Date'], date_today_str)
+        self.assertEqual(res['activities'][0]['Total Calories'], 90)
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
