ea75e5250f2fcbb9de4076b58593af60850ef82e
adriangohjw <adriangohjw@gmail.com>
2020-04-17 11:06:33 +0800

    fix bug in route_contracts (and update UT)

--- contracts/tests/test_route_contracts.py
+++ contracts/tests/test_route_contracts.py
@@ -41,6 +41,7 @@ class Test_route_contracts(unittest.TestCase):
         with self.assertRaises(TypeError):
             validate_distance(None)
             validate_distance(1)
+            validate_distance('')
 
     
     def test_validate_purpose(self):
@@ -66,8 +67,6 @@ class Test_route_contracts(unittest.TestCase):
         with self.assertRaises(TypeError):
             validate_calories(None)
             validate_calories(1)
-
-        with self.assertRaises(ValueError):
             validate_calories('')
 
 
@@ -76,6 +75,7 @@ class Test_route_contracts(unittest.TestCase):
         with self.assertRaises(TypeError):
             validate_ascent(None)
             validate_ascent(1)
+            validate_ascent('')
 
 
     def test_validate_descent(self):
@@ -83,6 +83,7 @@ class Test_route_contracts(unittest.TestCase):
         with self.assertRaises(TypeError):
             validate_descent(None)
             validate_descent(1)
+            validate_descent('')
 
 
     def test_route_read_contract(self):
@@ -122,11 +123,26 @@ class Test_route_contracts(unittest.TestCase):
                 }
             )
 
+        with app.test_request_context('/?username=johndoe&purpose=casual&polyline=test&startPos_latitude=50.0&startPos_longtitude=60.0&endPos_latitude=70.0&endPos_longtitude=80.0', method='POST'):
+            self.assertEqual(
+                route_create_contract(request), 
+                {
+                    'username': 'johndoe',
+                    'distance': 0,
+                    'purpose': 'casual',
+                    'calories': 0,
+                    'polyline': 'test',
+                    'ascent': 0,
+                    'descent': 0,
+                    'startPos_latitude': 50.0,
+                    'startPos_longtitude': 60.0,
+                    'endPos_latitude': 70.0,
+                    'endPos_longtitude': 80.0
+                }
+            )
+
         with app.test_request_context('/?username=&distance=&purpose=&calories=&polyline=&ascent=&descent=&startPos_latitude=&startPos_longtitude=&endPos_latitude=&endPos_longtitude=', method='POST'):
             self.assertRaises(ValueError, route_create_contract, request)
-
-        with app.test_request_context('/?username=johndoe&distance=hello&purpose=casual&calories=20&polyline=test&ascent=30&descent=40&startPos_latitude=50.0&startPos_longtitude=60.0&endPos_latitude=70.0&endPos_longtitude=80.0', method='POST'):
-            self.assertRaises(TypeError, route_create_contract, request)
 
 
     def test_route_update_contract(self):
