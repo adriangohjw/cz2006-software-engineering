2144c8c29e5d148930f2535fc0e572048bdd9e80
adriangohjw <adriangohjw@gmail.com>
2020-04-17 14:22:29 +0800

    add DAO (and UT) for stat

---
+++ dao/stat_dao.py
@@ -0,0 +1,27 @@
+import sys
+from os import path, getcwd
+sys.path.append(getcwd())
+
+from sqlalchemy import asc, desc, cast, Float, func, Numeric, Date
+from sqlalchemy.sql.functions import sum
+
+from models import db, Route
+
+import datetime 
+
+
+def dailyCaloriesRead(user_id):
+
+    query = db.session.query(
+        cast(Route.created_at, Date).label('Date'),
+        sum(Route.calories).label('total_calories')
+    )\
+    .filter(Route.user_id == user_id)\
+    .group_by(
+        cast(Route.created_at, Date)
+    )\
+    .order_by(
+        cast(Route.created_at, Date)
+    )
+
+    return query.all()
